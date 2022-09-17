81a7779bfc5de5d40751035ff3e4297516d72f19
shambhavi1923 <108674525+shambhavi1923@users.noreply.github.com>
2022-09-17 11:16:42 +0530

    Update gradingindict.py

--- gradingindict.py
+++ gradingindict.py
@@ -5,13 +5,7 @@ student_scores = {
   "Draco": 74,
   "Neville": 62,
 }
-
-
-
 student_grades ={}
-
-
-
 for student in student_scores:
   score = student_scores[student]
   if score > 90:
@@ -22,11 +16,6 @@ for student in student_scores:
     student_grades[student] = "Acceptable"
   else:
     student_grades[student] = "Fail"
-
-
-    
-
-
 print(student_grades)
 
 
